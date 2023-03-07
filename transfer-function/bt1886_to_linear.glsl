c36ab16cbce33e09acc76f32bbc7d8c11998330e
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2023-03-07 20:47:52 +0800

    Format BT.1886

--- transfer-function/bt1886_to_linear.glsl
+++ transfer-function/bt1886_to_linear.glsl
@@ -2,12 +2,13 @@
 //!BIND HOOKED
 //!DESC bt.1886 to linear
 
-const float DISPGAMMA = 2.4;
+const float GAMMA = 2.4;
 const float L_W = 1.0;
 const float L_B = 0.0;
+
+// The reference EOTF specified in Rec. ITU-R BT.1886
+// L = a(max[(V+b),0])^g
 float bt1886_f(float V, float gamma, float Lw, float Lb) {
-    // The reference EOTF specified in Rec. ITU-R BT.1886
-    // L = a(max[(V+b),0])^g
     float a = pow(pow(Lw, 1.0 / gamma) - pow(Lb, 1.0 / gamma), gamma);
     float b = pow(Lb, 1.0 / gamma) / (pow(Lw, 1.0 / gamma) - pow(Lb, 1.0 / gamma));
     float L = a * pow(max(V + b, 0.0), gamma);
@@ -16,11 +17,10 @@ float bt1886_f(float V, float gamma, float Lw, float Lb) {
 
 vec4 color = HOOKED_tex(HOOKED_pos);
 vec4 hook() {
-    // Encode linear code values with transfer function
     color.rgb = vec3(
-        bt1886_f(color.r, DISPGAMMA, L_W, L_B),
-        bt1886_f(color.g, DISPGAMMA, L_W, L_B),
-        bt1886_f(color.b, DISPGAMMA, L_W, L_B)
+        bt1886_f(color.r, GAMMA, L_W, L_B),
+        bt1886_f(color.g, GAMMA, L_W, L_B),
+        bt1886_f(color.b, GAMMA, L_W, L_B)
     );
     return color;
 }
--- transfer-function/linear_to_bt1886.glsl
+++ transfer-function/linear_to_bt1886.glsl
@@ -2,12 +2,13 @@
 //!BIND HOOKED
 //!DESC linear to bt.1886
 
-const float DISPGAMMA = 2.4;
+const float GAMMA = 2.4;
 const float L_W = 1.0;
 const float L_B = 0.0;
+
+// The reference EOTF specified in Rec. ITU-R BT.1886
+// L = a(max[(V+b),0])^g
 float bt1886_r(float L, float gamma, float Lw, float Lb) {
-    // The reference EOTF specified in Rec. ITU-R BT.1886
-    // L = a(max[(V+b),0])^g
     float a = pow(pow(Lw, 1.0 / gamma) - pow(Lb, 1.0 / gamma), gamma);
     float b = pow(Lb, 1.0 / gamma) / (pow(Lw, 1.0 / gamma) - pow(Lb, 1.0 / gamma));
     float V = pow(max(L / a, 0.0), 1.0 / gamma) - b;
@@ -16,11 +17,10 @@ float bt1886_r(float L, float gamma, float Lw, float Lb) {
 
 vec4 color = HOOKED_tex(HOOKED_pos);
 vec4 hook() {
-    // Encode linear code values with transfer function
     color.rgb = vec3(
-        bt1886_r(color.r, DISPGAMMA, L_W, L_B),
-        bt1886_r(color.g, DISPGAMMA, L_W, L_B),
-        bt1886_r(color.b, DISPGAMMA, L_W, L_B)
+        bt1886_r(color.r, GAMMA, L_W, L_B),
+        bt1886_r(color.g, GAMMA, L_W, L_B),
+        bt1886_r(color.b, GAMMA, L_W, L_B)
     );
     return color;
 }
