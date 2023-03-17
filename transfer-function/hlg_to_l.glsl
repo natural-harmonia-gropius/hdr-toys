8173baa6f87ced5c992ca6a763fe24a326c2dff1
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2023-03-17 22:15:33 +0800

    Format hlg

--- transfer-function/hlg_to_l.glsl
+++ transfer-function/hlg_to_l.glsl
@@ -4,17 +4,17 @@
 
 const float L_w   = 1000.0;
 const float L_b   = 0.0;
+
 const float alpha = L_w - L_b;
 const float beta  = L_b;
-const float gamma = 1.2;
+const float gamma = 1.2 * pow(1.111, log2(L_w / 1000.0));
 
 const float a = 0.17883277;
-const float b = 0.28466892;  // 1 - 4 * a;
-const float c = 0.55991073;  // 0.5 - a * log(4 * a);
+const float b = 1.0 - 4.0 * a;
+const float c = 0.5 - a * log(4.0 * a);
 
+// HLG EOTF (i.e. HLG inverse OETF followed by the HLG OOTF)
 vec3 HLG_to_Y(vec3 HLG) {
-    // HLG EOTF (i.e. HLG inverse OETF followed by the HLG OOTF)
-
     // HLG Inverse OETF scene-linear (non-linear signal value to scene linear)
     const vec3 sceneLinear = vec3(
         HLG.r >= 0.0 && HLG.r <= 0.5 ? pow(HLG.r, 2.0) / 3.0 : (exp((HLG.r - c) / a) + b) / 12.0,
--- transfer-function/l_to_hlg.glsl
+++ transfer-function/l_to_hlg.glsl
@@ -4,23 +4,23 @@
 
 const float L_w   = 1000.0;
 const float L_b   = 0.0;
+
 const float alpha = L_w - L_b;
 const float beta  = L_b;
-const float gamma = 1.2;
+const float gamma = 1.2 * pow(1.111, log2(L_w / 1000.0));
 
 const float a = 0.17883277;
-const float b = 0.28466892;  // 1 - 4 * a;
-const float c = 0.55991073;  // 0.5 - a * log(4 * a);
+const float b = 1.0 - 4.0 * a;
+const float c = 0.5 - a * log(4.0 * a);
 
+// HLG Inverse EOTF (i.e. HLG inverse OOTF followed by the HLG OETF)
 vec3 Y_to_HLG(vec3 displayLinear) {
-    // HLG Inverse EOTF (i.e. HLG inverse OOTF followed by the HLG OETF)
-
     // HLG Inverse OOTF (display linear to scene linear)
+    // This case is to protect against pow(0,-N)=Inf error. The ITU document
+    // does not offer a recommendation for this corner case. There may be a
+    // better way to handle this, but for now, this works.
     const float Y_d = dot(displayLinear, vec3(0.2627, 0.6780, 0.0593));
     const vec3 sceneLinear = vec3(
-        // This case is to protect against pow(0,-N)=Inf error. The ITU document
-        // does not offer a recommendation for this corner case. There may be a
-        // better way to handle this, but for now, this works.
         Y_d == 0.0 ? 0.0 : pow((Y_d - beta) / alpha, (1.0 - gamma) / gamma) * ((displayLinear.r - beta) / alpha),
         Y_d == 0.0 ? 0.0 : pow((Y_d - beta) / alpha, (1.0 - gamma) / gamma) * ((displayLinear.g - beta) / alpha),
         Y_d == 0.0 ? 0.0 : pow((Y_d - beta) / alpha, (1.0 - gamma) / gamma) * ((displayLinear.b - beta) / alpha)
