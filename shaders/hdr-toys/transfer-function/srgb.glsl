38bbd55831a81d5359911be14f8f07d243555b34
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2024-03-01 13:21:04 +0000

    Update transfer-function/srgb

--- shaders/hdr-toys/transfer-function/srgb.glsl
+++ shaders/hdr-toys/transfer-function/srgb.glsl
@@ -5,24 +5,27 @@
 //!BIND HOOKED
 //!DESC transfer function (srgb)
 
-float monitor_curve_eotf_inv(float y, float gamma, float offset) {
+const float gamma = 2.4;
+const float offset = 0.055;
+
+float monitor_curve_eotf_inv(float y) {
     const float yb = pow(offset * gamma / ((gamma - 1.0) * (1.0 + offset)), gamma);
     const float rs = pow((gamma - 1.0) / offset, gamma - 1.0) * pow((1.0 + offset) / gamma, gamma);
     return y >= yb ? (1.0 + offset) * pow(y, 1.0 / gamma) - offset : y * rs;
 }
 
-vec3 monitor_curve_eotf_inv(vec3 color, float gamma, float offset) {
+vec3 monitor_curve_eotf_inv(vec3 color) {
     return vec3(
-        monitor_curve_eotf_inv(color.r, gamma, offset),
-        monitor_curve_eotf_inv(color.g, gamma, offset),
-        monitor_curve_eotf_inv(color.b, gamma, offset)
+        monitor_curve_eotf_inv(color.r),
+        monitor_curve_eotf_inv(color.g),
+        monitor_curve_eotf_inv(color.b)
     );
 }
 
 vec4 hook() {
     vec4 color = HOOKED_texOff(0);
 
-    color.rgb = monitor_curve_eotf_inv(color.rgb, 2.4, 0.055);
+    color.rgb = monitor_curve_eotf_inv(color.rgb);
 
     return color;
 }
--- shaders/hdr-toys/transfer-function/srgb_inv.glsl
+++ shaders/hdr-toys/transfer-function/srgb_inv.glsl
@@ -5,24 +5,27 @@
 //!BIND HOOKED
 //!DESC transfer function (srgb, inverse)
 
-float monitor_curve_eotf(float x, float gamma, float offset) {
+const float gamma = 2.4;
+const float offset = 0.055;
+
+float monitor_curve_eotf(float x) {
     const float fs = ((gamma - 1.0) / offset) * pow(offset * gamma / ((gamma - 1.0) * (1.0 + offset)), gamma);
     const float xb = offset / (gamma - 1.0);
     return x >= xb ? pow((x + offset) / (1.0 + offset), gamma) : x * fs;
 }
 
-vec3 monitor_curve_eotf(vec3 color, float gamma, float offset) {
+vec3 monitor_curve_eotf(vec3 color) {
     return vec3(
-        monitor_curve_eotf(color.r, gamma, offset),
-        monitor_curve_eotf(color.g, gamma, offset),
-        monitor_curve_eotf(color.b, gamma, offset)
+        monitor_curve_eotf(color.r),
+        monitor_curve_eotf(color.g),
+        monitor_curve_eotf(color.b)
     );
 }
 
 vec4 hook() {
     vec4 color = HOOKED_texOff(0);
 
-    color.rgb = monitor_curve_eotf(color.rgb, 2.4, 0.055);
+    color.rgb = monitor_curve_eotf(color.rgb);
 
     return color;
 }
