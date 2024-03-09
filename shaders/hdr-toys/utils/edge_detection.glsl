0db395fa220e10e951851b25c2315081e318c100
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2024-03-09 02:46:20 +0000

    Update utils/edge_detection

--- shaders/hdr-toys/utils/edge_detection.glsl
+++ shaders/hdr-toys/utils/edge_detection.glsl
@@ -2,21 +2,80 @@
 //!BIND HOOKED
 //!DESC edge detection
 
-// Positive Laplacian
-const mat3 k = mat3(
+#define tex HOOKED_texOff
+
+
+const mat3 prewitt_x = mat3(
+    1.0, 0.0, -1.0,
+    1.0, 0.0, -1.0,
+    1.0, 0.0, -1.0
+);
+
+const mat3 prewitt_y = mat3(
+     1.0,  1.0,  1.0,
+     0.0,  0.0,  0.0,
+    -1.0, -1.0, -1.0
+);
+
+const mat3 sobel_x = mat3(
+    1.0, 0.0, -1.0,
+    2.0, 0.0, -2.0,
+    1.0, 0.0, -1.0
+);
+
+const mat3 sobel_y = mat3(
+     1.0,  2.0,  1.0,
+     0.0,  0.0,  0.0,
+    -1.0, -2.0, -1.0
+);
+
+const mat3 laplacian_p = mat3(
     0.0,  1.0, 0.0,
     1.0, -4.0, 1.0,
     0.0,  1.0, 0.0
 );
-const uvec2 k_size = uvec2(3, 3);
-const vec2  k_size_h = vec2(k_size / 2);
+
+const mat3 laplacian_n = mat3(
+    0.0,  -1.0,  0.0,
+    -1.0,  4.0, -1.0,
+    0.0,  -1.0,  0.0
+);
+
+const float base        = 0.0;
+const uvec2 k_size      = uvec2(3, 3);
+const vec2  k_size_h    = vec2(k_size / 2);
+
+vec3 conv(mat3 k) {
+    vec3 x = vec3(base);
+    for (uint i = 0; i < k_size.x; i++)
+        for (uint j = 0; j < k_size.y; j++)
+            x += tex(vec2(j, i) - k_size_h).rgb * k[i][j];
+    return x;
+}
+
+vec3 prewitt() {
+    vec3 x = conv(prewitt_x);
+    vec3 y = conv(prewitt_y);
+    vec3 g = abs(sqrt(x * x + y * y));
+    return g;
+}
+
+vec3 sobel() {
+    vec3 x = conv(sobel_x);
+    vec3 y = conv(sobel_y);
+    vec3 g = abs(sqrt(x * x + y * y));
+    return g;
+}
+
+vec3 laplacian() {
+    vec3 x = conv(laplacian_p);
+    return x;
+}
 
 vec4 hook() {
     vec4 color = vec4(vec3(0.0), 1.0);
 
-    for (uint i = 0; i < k_size.x; i++)
-        for (uint j = 0; j < k_size.y; j++)
-            color.rgb += HOOKED_texOff(vec2(j, i) - k_size_h).rgb * k[i][j];
+    color.rgb = laplacian();
 
     return color;
 }
