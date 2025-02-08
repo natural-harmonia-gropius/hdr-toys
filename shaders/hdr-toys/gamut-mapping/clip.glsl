27eaddd1be5de8bc0a51a6670515f7662e718c8b
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2025-02-08 21:52:12 +0800

    General updates and fixes
    Tidied up comments
    Added BRAVIA white point
    Added Kang color temperature function
    Added multiple gamuts
    Deleted the trailing zero
    Corrected variables used in RGB_to_RGB

--- shaders/hdr-toys/gamut-mapping/clip.glsl
+++ shaders/hdr-toys/gamut-mapping/clip.glsl
@@ -1,4 +1,5 @@
-// RGB to RGB conversion, include chromatic adaptation transform
+// RGB to RGB conversion, includes chromatic adaptation transform
+// All coordinates are based on the CIE 1931 2° chromaticity diagram
 
 //!HOOK OUTPUT
 //!BIND HOOKED
@@ -10,7 +11,6 @@
 
 #define from    BT2020
 #define to      BT709
-
 
 // White points of standard illuminants
 // https://en.wikipedia.org/wiki/Standard_illuminant#White_points_of_standard_illuminants
@@ -28,24 +28,60 @@ const vec2 F2   = vec2(0.37208, 0.37529);
 const vec2 F7   = vec2(0.31292, 0.32933);
 const vec2 F11  = vec2(0.38052, 0.37713);
 const vec2 DCI  = vec2(0.31400, 0.35100);
+// It is also known as D60
 const vec2 ACES = vec2(0.32168, 0.33767);
+// Colour Matching Between OLED and CRT
+// https://www.sony.jp/products/catalog/FUN_WhitePaper_OLED_ColorMatching_V1_00.pdf
+const vec2 BRAVIA = vec2(0.3067, 0.318);
 
 // https://en.wikipedia.org/wiki/Standard_illuminant#Illuminant_series_D
-vec2 CIE_D(float CCT) {
-    // https://en.wikipedia.org/wiki/Planckian_locus
+vec2 CIE_D(float T) {
+    // Compensate for the loss caused by the accuracy difference between the old and new standards
     // c2 = 1.4387768775039337
-    CCT = (CCT * 1.4388) / 1.438;
+    // https://en.wikipedia.org/wiki/Planckian_locus#Planckian_locus_in_the_XYZ_color_space
+    T = (T * 1.4388) / 1.438;
 
-    CCT = clamp(CCT, 4000.0, 25000.0);
+    // This formula is applicable to temperatures ranging from 4000K to 25000K
+    T = clamp(T, 4000.0, 25000.0);
 
-    float t1 = 1000.0 / CCT;
+    float t1 = 1000.0 / T;
     float t2 = t1 * t1;
     float t3 = t1 * t2;
 
-    float x = CCT <= 7000.0
-        ? 0.244063 + 0.09911 * t1 + 2.9678 * t2 - 4.6070 * t3
-        : 0.237040 + 0.24748 * t1 + 1.9018 * t2 - 2.0064 * t3;
+    float x =
+        T <= 7000.0
+            ? 0.244063 + 0.09911 * t1 + 2.9678 * t2 - 4.607 * t3
+            : 0.23704 + 0.24748 * t1 + 1.9018 * t2 - 2.0064 * t3;
+
+    // Daylight locus
     float y = -0.275 + 2.87 * x - 3.0 * x * x;
+
+    return vec2(x, y);
+}
+
+// https://en.wikipedia.org/wiki/Planckian_locus#Approximation
+vec2 Kang(float T) {
+    // This formula is applicable to temperatures ranging from 1667K to 25000K
+    T = clamp(T, 1667.0, 25000.0);
+
+    float t1 = 1000.0 / T;
+    float t2 = t1 * t1;
+    float t3 = t1 * t2;
+
+    float x =
+        T <= 4000.0
+            ? -0.2661239 * t3 - 0.234358 * t2 + 0.8776956 * t1 + 0.17991
+            : -3.0258469 * t3 + 2.1070379 * t2 + 0.2226347 * t1 + 0.24039;
+
+    float x2 = x * x;
+    float x3 = x2 * x;
+
+    float y =
+        T <= 2222.0
+            ? -1.1063814 * x3 - 1.3481102 * x2 - 2.18555832 * x - 0.20219683
+            : T <= 4000.0
+            ? -0.9549476 * x3 - 1.37418593 * x2 - 2.09137015 * x - 0.16748867
+            : 3.081758 * x3 - 5.8733867 * x2 + 3.75112997 * x - 0.37001483;
 
     return vec2(x, y);
 }
@@ -218,11 +254,51 @@ const Chromaticity AP1 = Chromaticity(
     ACES
 );
 
+// ARRI Wide Gamut 3
+const Chromaticity AWG3 = Chromaticity(
+    vec2(0.684,   0.313),
+    vec2(0.221,   0.848),
+    vec2(0.0861, -0.102),
+    D65
+);
+
 // ARRI Wide Gamut 4
 const Chromaticity AWG4 = Chromaticity(
     vec2(0.7347,  0.2653),
     vec2(0.1424,  0.8576),
     vec2(0.0991, -0.0308),
+    D65
+);
+
+// RED Wide Gamut RGB
+const Chromaticity RWG = Chromaticity(
+    vec2(0.780308,  0.304253),
+    vec2(0.121595,  1.493994),
+    vec2(0.095612, -0.084589),
+    D65
+);
+
+// DaVinci Wide Gamut
+const Chromaticity DWG = Chromaticity(
+    vec2(0.8000,  0.3130),
+    vec2(0.1682,  0.9877),
+    vec2(0.0790, -0.1155),
+    D65
+);
+
+// FilmLight E-Gamut
+const Chromaticity EGAMUT = Chromaticity(
+    vec2(0.8000,  0.3177),
+    vec2(0.1800,  0.9000),
+    vec2(0.0650, -0.0805),
+    D65
+);
+
+// FilmLight E-Gamut 2
+const Chromaticity EGAMUT2 = Chromaticity(
+    vec2(0.8300,  0.3100),
+    vec2(0.1500,  0.9500),
+    vec2(0.0650, -0.0805),
     D65
 );
 
@@ -269,34 +345,35 @@ const Chromaticity DGAMUT = Chromaticity(
 // Chromatic adaptation transform
 // https://en.wikipedia.org/wiki/LMS_color_space
 
+// It is also known as von Kries
 const mat3 HPE = mat3(
-     0.4002400,  0.7076000, -0.0808100,
-    -0.2263000,  1.1653200,  0.0457000,
-     0.0000000,  0.0000000,  0.9182200
+     0.40024,  0.70760, -0.08081,
+    -0.22630,  1.16532,  0.04570,
+     0.00000,  0.00000,  0.91822
 );
 
 const mat3 Bradford = mat3(
-     0.8951000,  0.2664000, -0.1614000,
-    -0.7502000,  1.7135000,  0.0367000,
-     0.0389000, -0.0685000,  1.0296000
+     0.8951,  0.2664, -0.1614,
+    -0.7502,  1.7135,  0.0367,
+     0.0389, -0.0685,  1.0296
 );
 
 const mat3 CAT97 = mat3(
-     0.8562000,  0.3372000, -0.1934000,
-    -0.8360000,  1.8327000,  0.0033000,
-     0.0357000, -0.0469000,  1.0112000
+     0.8562,  0.3372, -0.1934,
+    -0.8360,  1.8327,  0.0033,
+     0.0357, -0.0469,  1.0112
 );
 
 const mat3 CAT02 = mat3(
-     0.7328000,  0.4296000, -0.1624000,
-    -0.7036000,  1.6975000,  0.0061000,
-     0.0030000,  0.0136000,  0.9834000
+     0.7328,  0.4296, -0.1624,
+    -0.7036,  1.6975,  0.0061,
+     0.0030,  0.0136,  0.9834
 );
 
 const mat3 CAT16 = mat3(
-     0.4012880,  0.6501730, -0.0514610,
-    -0.2502680,  1.2044140,  0.0458540,
-    -0.0020790,  0.0489520,  0.9531270
+     0.401288,  0.650173, -0.051461,
+    -0.250268,  1.204414,  0.045854,
+    -0.002079,  0.048952,  0.953127
 );
 
 // Other constants
@@ -313,8 +390,7 @@ const mat3 SingularY3 = mat3(
     0.0, 1.0, 0.0
 );
 
-// Constants End
-
+// http://www.brucelindbloom.com/Eqn_xyY_to_XYZ.html
 vec3 xyY_to_XYZ(vec3 xyY) {
     float x = xyY.x;
     float y = xyY.y;
@@ -329,7 +405,22 @@ vec3 xyY_to_XYZ(vec3 xyY) {
     return vec3(X, Y, Z);
 }
 
-// http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+// http://www.brucelindbloom.com/Eqn_XYZ_to_xyY.html
+vec3 XYZ_to_xyY(vec3 XYZ) {
+    float X = XYZ.x;
+    float Y = XYZ.y;
+    float Z = XYZ.z;
+
+    float divisor = X + Y + Z;
+    if (divisor == 0.0) divisor = 1e-6;
+
+    float x = X / divisor;
+    float y = Y / divisor;
+
+    return vec3(x, y, Y);
+}
+
+// http://www.brucelindbloom.com/Eqn_RGB_XYZ_Matrix.html
 mat3 RGB_to_XYZ(Chromaticity C) {
     if (C == GRAY)
         return Identity3;
@@ -358,7 +449,7 @@ mat3 XYZ_to_RGB(Chromaticity C) {
     return inverse(RGB_to_XYZ(C));
 }
 
-// http://www.brucelindbloom.com/index.html?Eqn_ChromAdapt.html
+// http://www.brucelindbloom.com/Eqn_ChromAdapt.html
 mat3 adaptation(vec2 w1, vec2 w2, mat3 cat) {
     vec3 src_xyz = xyY_to_XYZ(vec3(w1, 1.0));
     vec3 dst_xyz = xyY_to_XYZ(vec3(w2, 1.0));
@@ -376,23 +467,23 @@ mat3 adaptation(vec2 w1, vec2 w2, mat3 cat) {
     return cat * scale_diag * inverse(cat);
 }
 
+// CAM16 uses CAT16 as cat and equal-energy illuminant (E) as wt.
+// https://www.researchgate.net/publication/318152296_Comprehensive_color_solutions_CAM16_CAT16_and_CAM16-UCS
+// Android uses Bradford as cat and D50 as wt.
+// https://android.googlesource.com/platform/frameworks/base/+/master/graphics/java/android/graphics/ColorSpace.java
 mat3 adaptation_two_step(vec2 w1, vec2 w2, vec2 wt, mat3 cat) {
     return adaptation(w1, wt, cat) * adaptation(wt, w2, cat);
 }
 
-// https://www.researchgate.net/publication/318152296_Comprehensive_color_solutions_CAM16_CAT16_and_CAM16-UCS
-// CAT16 uses equal-energy illuminant (E) as wt.
-// https://android.googlesource.com/platform/frameworks/base/+/master/graphics/java/android/graphics/ColorSpace.java
-// Android uses Bradford as cat and D50 as wt.
 mat3 adaptation_two_step(vec2 w1, vec2 w2) {
     return adaptation_two_step(w1, w2, E, CAT16);
 }
 
 mat3 RGB_to_RGB(Chromaticity c1, Chromaticity c2) {
     mat3 m = Identity3;
-    if (from != to) {
+    if (c1 != c2) {
         m *= RGB_to_XYZ(c1);
-        if (from.w != to.w) {
+        if (c1.w != c2.w) {
             m *= adaptation_two_step(c1.w, c2.w);
         }
         m *= XYZ_to_RGB(c2);
