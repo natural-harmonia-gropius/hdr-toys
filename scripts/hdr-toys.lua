45ee3dc00f61faed72b513744911d45a401cb562
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2025-02-17 17:26:09 +0800

    Replace hdr-toys.js with hdr-toys.lua

---
+++ scripts/hdr-toys.lua
@@ -0,0 +1,13 @@
+local options = require("mp.options")
+
+local o = {
+  temporal_stable_time = 1 / 3,
+}
+options.read_options(o, _, function() end)
+
+mp.observe_property("container-fps", "native", function (property, value)
+  if not value then return end
+  value = value * o.temporal_stable_time
+  value = math.floor(value + 0.5)
+  mp.command("no-osd set glsl-shader-opts temporal_stable_frames=" .. value)
+end)
