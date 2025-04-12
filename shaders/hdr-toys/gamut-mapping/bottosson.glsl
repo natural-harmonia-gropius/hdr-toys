02dc90182a5483f182fbad23f34df4e787efe01d
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2025-04-12 13:27:04 +0800

    General code organization

--- shaders/hdr-toys/gamut-mapping/bottosson.glsl
+++ shaders/hdr-toys/gamut-mapping/bottosson.glsl
@@ -5,7 +5,7 @@
 //!TYPE float
 //!MINIMUM 0.0
 //!MAXIMUM 1.0
-0.33
+0.3
 
 //!HOOK OUTPUT
 //!BIND HOOKED
@@ -15,77 +15,73 @@ float cbrt(float x) {
     return sign(x) * pow(abs(x), 1.0 / 3.0);
 }
 
+vec3 cbrt(vec3 color) {
+    return vec3(
+        cbrt(color.x),
+        cbrt(color.y),
+        cbrt(color.z)
+    );
+}
+
 vec3 RGB_to_XYZ(vec3 RGB) {
-    mat3 M = mat3(
+    return RGB * mat3(
         0.6369580483012914, 0.14461690358620832,  0.1688809751641721,
         0.2627002120112671, 0.6779980715188708,   0.05930171646986196,
-        0.000000000000000,  0.028072693049087428, 1.060985057710791);
-    return RGB * M;
+        0.000000000000000,  0.028072693049087428, 1.060985057710791
+    );
 }
 
 vec3 XYZ_to_RGB(vec3 XYZ) {
-    mat3 M = mat3(
+    return XYZ * mat3(
          1.716651187971268,  -0.355670783776392, -0.253366281373660,
         -0.666684351832489,   1.616481236634939,  0.0157685458139111,
-         0.017639857445311,  -0.042770613257809,  0.942103121235474);
-    return XYZ * M;
+         0.017639857445311,  -0.042770613257809,  0.942103121235474
+    );
 }
 
 vec3 XYZ_to_LMS(vec3 XYZ) {
-    mat3 M = mat3(
+    return XYZ * mat3(
         0.8190224379967030, 0.3619062600528904, -0.1288737815209879,
         0.0329836539323885, 0.9292868615863434,  0.0361446663506424,
-        0.0481771893596242, 0.2642395317527308,  0.6335478284694309);
-    return XYZ * M;
+        0.0481771893596242, 0.2642395317527308,  0.6335478284694309
+    );
 }
 
 vec3 LMS_to_XYZ(vec3 LMS) {
-    mat3 M = mat3(
+    return LMS * mat3(
          1.2268798758459243, -0.5578149944602171,  0.2813910456659647,
         -0.0405757452148008,  1.1122868032803170, -0.0717110580655164,
-        -0.0763729366746601, -0.4214933324022432,  1.5869240198367816);
-    return LMS * M;
+        -0.0763729366746601, -0.4214933324022432,  1.5869240198367816
+    );
 }
 
 vec3 LMS_to_Lab(vec3 LMS) {
-    mat3 M = mat3(
+    return LMS * mat3(
         0.2104542683093140,  0.7936177747023054, -0.0040720430116193,
         1.9779985324311684, -2.4285922420485799,  0.4505937096174110,
-        0.0259040424655478,  0.7827717124575296, -0.8086757549230774);
-
-    LMS = vec3(
-        cbrt(LMS.x),
-        cbrt(LMS.y),
-        cbrt(LMS.z)
+        0.0259040424655478,  0.7827717124575296, -0.8086757549230774
     );
-
-    return LMS * M;
 }
 
 vec3 Lab_to_LMS(vec3 Lab) {
-    mat3 M = mat3(
+    return Lab * mat3(
         1.0000000000000000,  0.3963377773761749,  0.2158037573099136,
         1.0000000000000000, -0.1055613458156586, -0.0638541728258133,
-        1.0000000000000000, -0.0894841775298119, -1.2914855480194092);
-
-    Lab = Lab * M;
-
-    return vec3(
-        pow(Lab.x, 3.0),
-        pow(Lab.y, 3.0),
-        pow(Lab.z, 3.0)
+        1.0000000000000000, -0.0894841775298119, -1.2914855480194092
     );
 }
 
 vec3 RGB_to_Lab(vec3 color) {
     color = RGB_to_XYZ(color);
     color = XYZ_to_LMS(color);
+    color = cbrt(color);
     color = LMS_to_Lab(color);
     return color;
 }
 
 vec3 Lab_to_RGB(vec3 color) {
     color = Lab_to_LMS(color);
+    color = pow(color, vec3(3.0));
     color = LMS_to_XYZ(color);
     color = XYZ_to_RGB(color);
     return color;
@@ -93,28 +89,30 @@ vec3 Lab_to_RGB(vec3 color) {
 
 const float epsilon = 1e-6;
 
-vec3 Lab_to_LCH(vec3 Lab) {
+vec3 Lab_to_LCh(vec3 Lab) {
+    float L = Lab.x;
     float a = Lab.y;
     float b = Lab.z;
 
     float C = length(vec2(a, b));
-    float H = 0.0;
+    float h = (abs(a) < epsilon && abs(b) < epsilon) ? 0.0 : atan(b, a);
 
-    if (!(abs(a) < epsilon && abs(b) < epsilon))
-        H = atan(b, a);
-
-    return vec3(Lab.x, C, H);
+    return vec3(L, C, h);
 }
 
-vec3 LCH_to_Lab(vec3 LCH) {
-    float C = max(LCH.y, 0.0);
-    float H = LCH.z;
+vec3 LCh_to_Lab(vec3 LCh) {
+    float L = LCh.x;
+    float C = LCh.y;
+    float h = LCh.z;
 
-    float a = C * cos(H);
-    float b = C * sin(H);
+    C = max(C, 0.0);
+    float a = C * cos(h);
+    float b = C * sin(h);
 
-    return vec3(LCH.x, a, b);
+    return vec3(L, a, b);
 }
+
+
 
 
 
@@ -320,25 +318,33 @@ vec3 softClipColor(vec3 color) {
 
 
 
+
+
 vec4 hook() {
     vec4 color = HOOKED_tex(HOOKED_pos);
 
-    vec3 oklch = Lab_to_LCH(RGB_to_Lab(color.rgb));
-    float L = oklch.x;
-    float C = oklch.y;
-    float h = oklch.z;
+    vec3 lch = Lab_to_LCh(RGB_to_Lab(color.rgb));
 
-    if (L >= 1.0)
-        return vec4(vec3(1.0, 1.0, 1.0), color.a);
-    if (L <= 0.0)
-        return vec4(vec3(0.0, 0.0, 0.0), color.a);
+    float L = lch.x;
+    float C = lch.y;
+    float h = lch.z;
 
-    if (C > 1e-6) {
-        vec2 ST = approximateShape();
-        float C_smooth = (1.0 / ((ST.x / L) + (ST.y / max(1.0 - L, 1e-6))));
-        color.rgb = compute(L, h, C / sqrt(C * C / C_smooth / C_smooth + 1.0));
-        color.rgb = softClipColor(color.rgb);
+    // if (L >= 1.0) {
+    //     return vec4(vec3(1.0, 1.0, 1.0), color.a);
+    // }
+
+    // if (L <= 0.0) {
+    //     return vec4(vec3(0.0, 0.0, 0.0), color.a);
+    // }
+
+    if (C <= 1e-6) {
+        return color;
     }
+
+    vec2 ST = approximateShape();
+    float C_smooth = (1.0 / ((ST.x / L) + (ST.y / max(1.0 - L, 1e-6))));
+    color.rgb = compute(L, h, C / sqrt(C * C / C_smooth / C_smooth + 1.0));
+    color.rgb = softClipColor(color.rgb);
 
     return color;
 }
