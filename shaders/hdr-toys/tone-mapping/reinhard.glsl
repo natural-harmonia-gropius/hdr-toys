c5b1e0e98748cf431e21a7d355e022c473e4dabc
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2025-12-30 22:04:05 +0800

    Update reinhard
    apply black point compensation directly to the curve

--- shaders/hdr-toys/tone-mapping/reinhard.glsl
+++ shaders/hdr-toys/tone-mapping/reinhard.glsl
@@ -158,6 +158,26 @@ float get_max_l() {
     return 1000.0;
 }
 
+float f_slope(float x0, float y0, float x1, float y1) {
+    float num = (y1 - y0);
+    float den = (x1 - x0);
+    return abs(den) < 1e-6 ? 1.0 : num / den;
+}
+
+float f_intercept(float slope, float x0, float y0) {
+    return y0 - slope * x0;
+}
+
+float f_linear(float x, float slope, float intercept) {
+    return slope * x + intercept;
+}
+
+float f_bpc(float x, float x0, float y0, float x1, float y1) {
+    float slope = f_slope(x0, y0, x1, y1);
+    float intercept = f_intercept(slope, x0, y0);
+    return f_linear(x, slope, intercept);
+}
+
 float f(float x, float w) {
     float simple = x / (1.0 + x);
     float extended = simple * (1.0 + x / (w * w));
@@ -166,7 +186,9 @@ float f(float x, float w) {
 
 float curve(float x) {
     float w = get_max_l();
-    return f(x, w);
+    float y = f(x, w);
+    float y_bpc = f_bpc(y, 0.0, 0.001, 1.0, 1.0);
+    return y_bpc;
 }
 
 vec2 chroma_correction(vec2 ab, float i1, float i2) {
@@ -187,43 +209,6 @@ vec4 hook() {
     color.rgb = RGB_to_ICtCp(color.rgb);
     color.rgb = tone_mapping(color.rgb);
     color.rgb = ICtCp_to_RGB(color.rgb);
-
-    return color;
-}
-
-//!HOOK OUTPUT
-//!BIND HOOKED
-//!DESC black point compensation
-
-// https://www.color.org/WP40-Black_Point_Compensation_2010-07-27.pdf
-
-vec3 RGB_to_XYZ(vec3 RGB) {
-    return RGB * mat3(
-        0.6369580483012914, 0.14461690358620832,  0.1688809751641721,
-        0.2627002120112671, 0.6779980715188708,   0.05930171646986196,
-        0.000000000000000,  0.028072693049087428, 1.060985057710791
-    );
-}
-
-vec3 XYZ_to_RGB(vec3 XYZ) {
-    return XYZ * mat3(
-         1.716651187971268,  -0.355670783776392, -0.253366281373660,
-        -0.666684351832489,   1.616481236634939,  0.0157685458139111,
-         0.017639857445311,  -0.042770613257809,  0.942103121235474
-    );
-}
-
-vec3 black_point_compensation(vec3 XYZ, float s, float d) {
-    float r = (1.0 - d) / (1.0 - s);
-    return r * XYZ + (1.0 - r) * RGB_to_XYZ(vec3(1.0));
-}
-
-vec4 hook() {
-    vec4 color = HOOKED_tex(HOOKED_pos);
-
-    color.rgb = RGB_to_XYZ(color.rgb);
-    color.rgb = black_point_compensation(color.rgb, 0.0, 0.001);
-    color.rgb = XYZ_to_RGB(color.rgb);
 
     return color;
 }
